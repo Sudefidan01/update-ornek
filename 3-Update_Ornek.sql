4257b045104275a204276d1d8c340e7f39960c0f
sude <sudefidan@icloud.com>
2024-10-16 00:52:19 +0300

    update-ornek

---
+++ 3-Update_Ornek.sql
@@ -0,0 +1,5 @@
+--Öğrenciler tablosu içerisinde Primary Key Kolon bilgisi 2 olan öğrencinin not bilgilerini güncelleyelim
+Update Ogrenciler set Not1=75 , Not2=90
+where Id=2
+
+select * from Ogrenciler where Id=2
